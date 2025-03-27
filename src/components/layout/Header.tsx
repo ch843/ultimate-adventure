a4aa98bee83fca6a242c280aa4280520bde13a5f
Cassidy Hardisty <ch843@byu.edu>
2025-03-27 15:28:28 -0600

    react router

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,22 @@
 import {FC} from "react";
+import {Link} from 'react-router-dom';
 
 const Header: FC = () => {
     return (
-        <></>
+        <nav>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/book">Book Your Adventure</Link>
+            </div>
+            <div >
+                <Link to="/reviews">Reviews</Link>
+            </div>
+            <div >
+                <Link to="/about">About</Link>
+            </div>
+        </nav>
     )
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,13 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 import Header from "./Header.tsx";
 import Footer from "./Footer";
+import {Outlet} from "react-router-dom";
 
-interface LayoutProps {
-    children: ReactNode;
-}
-
-const Layout: FC<LayoutProps> = ({ children }) => {
+const Layout: FC= () => {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div>
             <Header />
-            <main className="flex-1 p-4">{children}</main>
+            <Outlet />
             <Footer />
         </div>
     );
