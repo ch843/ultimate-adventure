3c17b0f3502cca4f721764bde9afd5475b55578d
Cassidy Hardisty <ch843@byu.edu>
2025-04-04 19:36:25 -0600

    Fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: ["./src/components/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        accent: "#295B5B",
-        cream: "#FFE6C5",
-        link: "#D67C5C",
-        nav: "#191D2B",
-      },
-    },
-  },
+  content: [
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {},
   plugins: [],
 }
 
