0381e958d80cb61c08739c10fbba811e35354ac3
Connor Ellis <ellisconnor88@gmail.com>
2025-05-12 21:55:13 -0600

    Fixed home buttons

--- src/components/sections/CompanyFeature.tsx
+++ src/components/sections/CompanyFeature.tsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const CompanyFeature = () => {
   return (
     <section className="py-16 bg-white">
@@ -8,9 +10,9 @@ const CompanyFeature = () => {
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 shadow-lg">
           <div className='py-20 flex justify-center items-center' style={{backgroundImage: "url('https://ooelvqpdhbpsjsqbrljg.supabase.co/storage/v1/object/public/ultimate-adventure-prod//rappelling1.avif')"}}>
-                <button type="button" className='bg-emerald-800 text-white rounded-2xl p-8 text-xl shadow-lg'>
+                <Link to="/about" type="button" className='bg-emerald-800 text-white rounded-2xl p-8 text-xl shadow-lg'>
                   ABOUT US
-                </button>
+                </Link>
           </div>
           <div className='bg-orange-100 py-14 px-14 text-2xl font-light tracking-wide leading-loose text-center'>
             With over 30 years of experience, Ultimate Adventure Guides
@@ -20,9 +22,9 @@ const CompanyFeature = () => {
             to some of our favorite past destinations.
           </div>
           <div className='py-20 flex justify-center items-center' style={{backgroundImage: "url('https://ooelvqpdhbpsjsqbrljg.supabase.co/storage/v1/object/public/ultimate-adventure-prod//rappelling2.avif')"}}>
-              <button type="button" className='bg-orange-100 text-gray-700 rounded-2xl py-8 px-10 text-xl shadow-lg'>
+              <Link to="/book" type="button" className='bg-orange-100 text-gray-700 rounded-2xl py-8 px-10 text-xl shadow-lg'>
                 JOIN US
-              </button>
+              </Link>
           </div>
         </div>
       </div>
