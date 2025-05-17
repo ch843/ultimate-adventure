c73dd09b729b69fc8daf41116073b14f2ff7917a
Cassidy Hardisty <ch843@byu.edu>
2025-05-17 13:47:18 -0600

    Fixed vercel issues maybe

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,8 +10,8 @@ const Footer: FC = () => {
                     <div className="flex justify-between">
                         <div className="flex flex-col items-start">
                             <h1 className="text-white text-2xl pb-5">Ultimate Adventure Guides</h1>
-                            <Link to="/book" className="text-white text-xl">BOOK</Link>
-                            <Link to="/" className="text-white text-xl">PAY SHARON</Link>
+                            <Link to="/book" className="text-gray-300 text-lg pb-1">BOOK NOW</Link>
+                            <Link to="/" className="text-gray-300 text-lg">PAY SHARON</Link>
                         </div>
                         <div className="flex items-center">
                             <Link to="/">
